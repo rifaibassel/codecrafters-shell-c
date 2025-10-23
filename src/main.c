bbc85f7837f8d87dc7c3ca9c434b41e35f0dc2d9
Ahmad Alrefai <afa5959@psu.edu>
2025-10-23 18:45:45 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
+  char input[100];
   // Uncomment this block to pass the first stage
   printf("$ ");
+  scanf("%s", input);
+  printf("%s: command not found", input);
 
   return 0;
 }
