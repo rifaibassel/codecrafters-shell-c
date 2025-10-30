51f5f89889b0a4ea794f0704214d89efe6fd2917
Ahmad Alrefai <afa5959@psu.edu>
2025-10-30 15:01:09 -0400

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -7,16 +7,25 @@ int main(int argc, char *argv[]) {
   setbuf(stdout, NULL);
 
   char input[100];
-  char exit_str[100] = "exit 0";
+  char exit_str[100] = "exit";
   char echo_str[100] = "echo";
   char type_str[100] = "type";
 
   do {
     printf("$ ");
     fgets(input, sizeof(input), stdin);
+
     input[strlen(input) - 1] = '\0';
-    if (strcmp(input, exit_str) == 0) {
-      exit(0);
+
+    if (strncmp(input, exit_str, 4) == 0) {
+      char *token = strtok(input, " ");
+      token = strtok(NULL, " ");
+
+      if (strcmp(token, "0") == 0) {
+        exit(0);
+      } else if (strcmp(token, "1")) {
+        exit(1);
+      }
     }
 
     if (strncmp(input, echo_str, 4) == 0) {
@@ -40,16 +49,15 @@ int main(int argc, char *argv[]) {
         printf("$");
       }
       while (token != NULL && strcmp(token, "(null)") != 0) {
-        if (strcmp(token, echo_str) == 0 || strcmp(token, exit_str) == 0 ||
+        if (strcmp(token, echo_str) == 0 || strncmp(token, exit_str, 4) == 0 ||
             strcmp(token, type_str) == 0) {
           printf("%s is a shell builtin\n", token);
           token = strtok(NULL, " ");
         } else {
-          printf("%s: command not found\n", token);
+          printf("%s: not found\n", token);
           token = strtok(NULL, " ");
         }
       }
-      printf("\n");
       continue;
     }
 
