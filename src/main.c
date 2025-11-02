80f7c870cb6863dbc01e80b4bf39f4f9e44974a7
Ahmad Alrefai <afa5959@psu.edu>
2025-11-02 15:54:17 -0500

    Put include in header files

--- src/main.c
+++ src/main.c
@@ -1,10 +1,4 @@
 #include "my_funcs.h"
-#include <fts.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
