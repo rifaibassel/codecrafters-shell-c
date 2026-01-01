fb67b4972d6d3119a66f3711e1215fc7d505a313
Ahmad Alrefai <afa5959@psu.edu>
2026-01-01 09:25:58 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
     printf("$ ");
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0';
-    printf("%s: command not found", command);
+    printf("%s: command not found\n", command);
 
   } while (1);
 
