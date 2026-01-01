4721df17138c23f9538c940d4fa2d7ce825d3f75
Ahmad Alrefai <afa5959@psu.edu>
2026-01-01 09:25:44 -0500

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
   char command[1024];
+  do {
+    printf("$ ");
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
+    printf("%s: command not found", command);
 
-  printf("$ ");
-  fgets(command, sizeof(command), stdin);
-  command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found", command);
+  } while (1);
 
   return 0;
 }
